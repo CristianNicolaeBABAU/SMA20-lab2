2d5bcd293894eb18e3c736d6aa919f6749e43245
Mălina Ivan <ivanmalina98@yahoo.com>
2020-10-12 14:44:46 +0300

    Create Lab2

--- IvanMalina/app/src/main/java/com/ivanmli/lab2/MainActivity.java
+++ IvanMalina/app/src/main/java/com/ivanmli/lab2/MainActivity.java
@@ -1,14 +1,120 @@
 package com.ivanmli.lab2;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AbsSpinner;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder alertDialogBuilder;
+    private Spinner spinner;
+    private Button bRandom;
+    private Uri contentUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bClick;
+        final TextView tName;
+        final EditText eName;
+        Button bShare;
+        Button bSearch;
+
+
+        bClick = (Button) findViewById(R.id.bClick);
+        tName = (TextView) findViewById(R.id.tName);
+        eName = (EditText) findViewById(R.id.eName);
+        bShare = (Button) findViewById(R.id.bShare);
+        bSearch = (Button) findViewById(R.id.bSearch);
+
+
+        bClick.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                tName.setText("Hello " + eName.getText() + "!");
+                alertDialogBuilder = new AlertDialog.Builder((MainActivity.this));
+                alertDialogBuilder.setTitle("Greetings!");
+                alertDialogBuilder.setMessage("Do you want to leave the aplication?");
+                alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Enjoy your staying!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Action canceled!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+                spinner = (Spinner) findViewById(R.id.spinner);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.colors, R.layout.support_simple_spinner_dropdown_item);
+                adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+
+
+            }
+        });
+
+       bShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+
+            }
+        });
+
+
+        bSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                    startActivity(browserIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "No application can handle this request." + " Please install a webrowser", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
